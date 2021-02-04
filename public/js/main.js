52ac0181b27e851576365e3b163ba794e85c5bbf
Aleksa Obradovic <obradovic.aleksa@gmail.com>
2021-02-04 13:34:43 +0100

    asd

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true
 });
 
-const socket = io();
+const socket = io('https://happy-albattani-6a00e9.netlify.app');
 
 // Join chatroom
 socket.emit('joinRoom', { username, room });
